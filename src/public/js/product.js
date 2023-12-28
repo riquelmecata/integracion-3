4581f96a1f7685f67ff0f74fe82211b3f8e73601
Catalina Riquelme <riquelmecata@gmail.com>
2023-12-28 02:44:32 -0300

    casi terminado

--- src/public/js/product.js
+++ src/public/js/product.js
@@ -1,4 +1,4 @@
-let addToCartButtons = document.querySelectorAll('.btn-outline-success');
+let addToCartButtons = document.querySelectorAll('.addToCart');
 
 addToCartButtons.forEach((button) => {
     button.addEventListener('click', async (event) => {
@@ -33,3 +33,37 @@ addToCartButtons.forEach((button) => {
         }
     });
 });
+
+let deleteProductBtn = document.querySelectorAll('.deleteProduct');
+
+deleteProductBtn.forEach((button) => {
+    button.addEventListener('click', async (event) => {
+        const productId = event.target.getAttribute('data-id');
+        console.log(productId);
+
+        try {
+            const user = await axios.get('http://localhost:8080/api/sessions/current');
+            const response = await axios.delete(`http://localhost:8080/users/premium/${user.data._id}/products/${productId}`);
+    
+            if (response.status === 200) {
+                await Swal.fire({
+                    title: 'Producto eliminado',
+                    text: 'El producto ha sido eliminado correctamente',
+                    icon: 'success',
+                    confirmButtonText: 'Entendido'
+                });
+    
+                location.replace("http://localhost:8080/products");
+            } else {
+                throw new Error('Error al eliminar el producto');
+            }
+        } catch (error) {
+            Swal.fire({
+                title: 'Error',
+                text: error.message || 'Hubo un problema al eliminar el producto',
+                icon: 'error',
+                confirmButtonText: 'Entendido'
+            });
+        }
+    });
+})    
